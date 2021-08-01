c5e2b70316df6c615bc4ac7981b21eff73b73b47
jiyoondev <jiyoon.jang.dev@gmail.com>
2021-08-01 20:20:05 +0900

    Add source1 automation functionality

--- app/src/main/java/com/jiyoon/fistclickerapp/MainActivity.kt
+++ app/src/main/java/com/jiyoon/fistclickerapp/MainActivity.kt
@@ -2,7 +2,13 @@ package com.jiyoon.fistclickerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.jiyoon.fistclickerapp.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,14 +20,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         // 기능 - (자원: 돈) 나타내기
-        var currentMoney = 0
+        var sumMoney = 0
+        var currentMoney = 1
+
+
+        // TODO: 타이머
+
+        // 핸들러
+        var handler = Handler(Looper.getMainLooper())
 
         // 기능 - 클릭 시 (자원: 돈) 증가
         binding.btnClick.setOnClickListener {
             Thread() {
                 currentMoney++
+                sumMoney += currentMoney
             }.start()
-            binding.tvMoney.text = "Money: $currentMoney"
+            binding.tvStartSource1.text = "Money: $currentMoney"
         }
+
+
+        // 기능 - shop1 함수
+        var source1IsClicked: Boolean = false
+        binding.btnSource1.setOnClickListener {
+            source1IsClicked = true
+        }
+
+        // 기능 - 클릭 시 (자원: 돈) 증가
+        Thread() {
+            var sumSource1 = 0
+            var currentSource1 = 0
+
+            kotlin.concurrent.timer(period = 1000, initialDelay = 1000) {
+                if (source1IsClicked) {
+                    currentSource1++
+                    sumSource1 += currentSource1
+
+                    handler.post {
+                        binding.tvSource1.text = "Bread: $currentSource1"
+                    }
+                }
+            }
+        }.start()
     }
 }
