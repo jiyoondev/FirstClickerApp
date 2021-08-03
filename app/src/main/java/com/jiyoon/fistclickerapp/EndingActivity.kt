5531eff0110dc314af32ca2dd72fd2ecdf02ba84
jiyoondev <jiyoon.jang.dev@gmail.com>
2021-08-04 04:33:33 +0900

    Add Ending Content & Refactoring

---
+++ app/src/main/java/com/jiyoon/fistclickerapp/EndingActivity.kt
@@ -0,0 +1,11 @@
+package com.jiyoon.fistclickerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EndingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ending)
+    }
+}
